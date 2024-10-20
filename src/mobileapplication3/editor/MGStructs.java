9ee22ecb1a7137576420c2cb980e038983b10a3d
vipoll <vipollmail@gmail.com>
2024-10-20 17:37:02 +0400

    use Logger.log() instead of System.out.println() and printStackTrace()

--- src/mobileapplication3/editor/MGStructs.java
+++ src/mobileapplication3/editor/MGStructs.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import mobileapplication3.editor.elements.Element;
 import mobileapplication3.platform.FileUtils;
+import mobileapplication3.platform.Logger;
 import mobileapplication3.platform.Utils;
 
 public class MGStructs {
@@ -15,34 +16,34 @@ public class MGStructs {
     public static Element[] readMGStruct(DataInputStream dis) {
         try {
             short fileVer = dis.readShort();
-            System.out.println("mgstruct v" + fileVer);
+            Logger.log("mgstruct v" + fileVer);
             short elementsCount = dis.readShort();
-            System.out.println("elements count: " + elementsCount);
+            Logger.log("elements count: " + elementsCount);
             Element[] elements = new Element[elementsCount];
             for (int i = 0; i < elementsCount; i++) {
                 elements[i] = readNextElement(dis);
                 if (elements[i] == null) {
-                    System.out.println("got null. stopping read");
+                    Logger.log("got null. stopping read");
                     return elements;
                 }
             }
             return elements;
         } catch (NullPointerException ex) {
-        	System.out.println("readMGStruct: caught NPE. nothing to read");
+        	Logger.log("readMGStruct: caught NPE. nothing to read");
         	return null;
         } catch (IOException ex) {
-            ex.printStackTrace();
+            Logger.log(ex);
             return null;
         }
     }
     
     public static Element readNextElement(DataInputStream is) {
-        System.out.println("reading next element...");
+        Logger.log("reading next element...");
         try {
             short id = is.readShort();
             System.out.print("id" + id + " ");
             if (id == 0) {
-                System.out.println("id0 is EOF mark. stopping");
+                Logger.log("id0 is EOF mark. stopping");
                 return null;
             }
             Element element = Element.createTypedInstance(id);
@@ -56,12 +57,12 @@ public class MGStructs {
                 args[i] = is.readShort();
             }
             
-            System.out.println(Utils.shortArrayToString(args));
+            Logger.log(Utils.shortArrayToString(args));
             
             element.setArgs(args);
             return element;
         } catch (IOException ex) {
-            ex.printStackTrace();
+            Logger.log(ex);
             return null;
         }
     }
